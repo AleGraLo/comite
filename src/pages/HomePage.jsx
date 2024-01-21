cfa90b00591cbb8d0104e33f3b5299e7b6c9fbae
alex@delcampoalacocina.com <alex@delcampoalacocina.com>
2024-01-21 22:58:16 +0100

    homepage img fondo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import '../CSS/style.css'; // Asegúrate de que tienes los estilos adecuados
-import workersImage from '../assets/DALL·E 2024-01-21 13.08.39 - A graphic design style image featuring a variety of workers in different environments. There are warehouse workers wearing gray overalls or shirts wit.png';
+// La importación de workersImage ya no es necesaria si la usas en CSS
 
 function HomePage() {
   return (
     <div className="homepage">
       <section className="banner">
         <h1 className="heading-primary">Bienvenidos al Comité de Bidafarma Málaga</h1>
-        <img src={workersImage} alt="Trabajadores" className="workers-image" />
-      </section>
-      <section className="content">
         <h2 className="heading-secondary">Nuestra Misión</h2>
         <p>En Comité Bidafarma Málaga, trabajamos juntos para mejorar nuestro entorno laboral y apoyar a cada uno de nuestros compañeros.</p>
+
       </section>
+
       {/* Añade más secciones según sea necesario */}
     </div>
   );
 }
+
 
 export default HomePage;
 
